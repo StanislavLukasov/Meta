e67f6ddac7509f5e66655bf317d01668437df1a2
Stanislav Lukasov <stasikest89@gmail.com>
2018-02-19 10:29:14 +0200

    Adds fetch package

--- src/components/Search/search.js
+++ src/components/Search/search.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import 'whatwg-fetch'
 import styles from './styles'
 import transformer from './transformer'
 import ImagesActions from '../../store/images/actions'
